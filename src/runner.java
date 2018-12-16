4224ffb9bd9437caf4ace974630bfaebc40067e6
Sheba Sow <ssow8171@bths.edu>
2018-12-16 17:45:03 -0500

    DONE

--- src/runner.java
+++ src/runner.java
@@ -6,6 +6,16 @@ public class runner {
         HiddenWord trail1 = new HiddenWord("APPLE");
         String hint = "";
         Scanner in = new Scanner(System.in);
-        System.out.println("What");
+        System.out.println("What's your guess?");
+        String guess = in.nextLine();
+        hint = trail1.getHint(guess);
+        while(!hint.equals("APPLE"))
+        {
+            System.out.println("Your hint: " + hint);
+            System.out.println("what's your next guess?");
+            guess = in.nextLine();
+            hint = trail1.getHint(guess);
+        }
+        System.out.println("congrats you got the secret word!");
     }
 }
